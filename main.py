770094567283ab5111833b489d32c1127d02e77b
tcadamson <taylor@catcat.dev>
2022-12-11 15:21:24 -0700

    Clean up old, malformed recap data and write to recap.db

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 import json
 import sqlite3
 
-db = sqlite3.connect(":memory:")
+db = sqlite3.connect("recap.db")
 
 def process_weekly_data(path):
     for game, entry_data in json.load(open(path)).items():
@@ -14,7 +14,7 @@ def process_weekly_data(path):
             filename_parts = re.sub("(:)([^.]*)", "\g<2>\g<1>", filename).split(":")
             if "default" in filename_parts[0]:
                 filename_parts = [re.sub("[.a-z]+", "", part) for part in filename_parts]
-            game_id = db.execute("SELECT game_id FROM games WHERE game=?", (game,)).fetchone()[0]
+            game_id = db.execute("SELECT game_id FROM games WHERE game = ?", (game,)).fetchone()[0]
             db.execute("INSERT INTO entries (game_id, stamp, ext, progress) VALUES (?, ?, ?, ?)", (game_id, filename_parts[0], filename_parts[1], progress))
 
 if __name__ == "__main__":
@@ -43,11 +43,28 @@ if __name__ == "__main__":
     for game, game_data in json.load(open("recap.agdg.io/res/games.json")).items():
         db.execute("INSERT INTO games (game, dev, tools, web) VALUES (?, ?, ?, ?)", (game, game_data["dev"], game_data["tools"], game_data["web"]))
     # Populate the entries table from all the old weekly data
-    for root, folders, files in os.walk("recap.agdg.io/res", topdown=True):
+    for root, folders, files in os.walk("recap.agdg.io/res", topdown = True):
         for folder in folders:
             process_weekly_data(f"{os.path.join(root, folder)}/data.json")
-    for entry in db.execute("SELECT * FROM entries LIMIT 10").fetchall():
-        print(entry)
+
+    # Bad entries due to user or script error (electing to keep some for "culture"):
+    # - (joke entry + script error)
+    db.execute("DELETE FROM games WHERE game_id = 449")
+    # No name yet (joke entry)
+    db.execute("DELETE FROM games WHERE game_id = 359")
+    # kitty (joke entry)
+    # ̴̞̮̘͘͜͝ (joke entry)
+    # 55 days (misspelling of 55 Days)
+    db.execute("UPDATE entries SET game_id = 551 WHERE game_id = 555")
+    db.execute("DELETE FROM games WHERE game_id = 555")
+    # td2s (misspelling of TD2S)
+    db.execute("UPDATE entries SET game_id = 544 WHERE game_id = 556")
+    db.execute("DELETE FROM games WHERE game_id = 556")
+    # uwdy (provisional) (script error)
+    db.execute("DELETE FROM games WHERE game_id = 484")
+    # Working title (old title of TRASH GAME AS IN TRASH MOVIE BUT ITS A GAME)
+    db.execute("UPDATE entries SET game_id = 180 WHERE game_id = 461")
+    db.execute("DELETE FROM games WHERE game_id = 461")
 
     # Commit to ensure there are no pending transactions before closing
     db.commit()
