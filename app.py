69dee82c975c7d5ad3ff5b5fc2f68cca0c84eac2
tcadamson <taylor@catcat.dev>
2023-01-21 05:37:08 -0700

    feat(app): add view template

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import time
+
 from flask import Flask, render_template
+
+import scraper
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", datestamp = scraper.decode_unix(time.time()))
+
+@app.route("/view/<datestamp>")
+def view(datestamp):
+    return render_template("view.html", datestamp = datestamp)
