6eb20b0c0a905f69c00a9e1ea1528144f31fc87b
tcadamson <taylor@catcat.dev>
2023-01-20 22:23:01 -0700

    feat(app): add home template and CSS reset

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("base.html")
+def home():
+    return render_template("home.html")
