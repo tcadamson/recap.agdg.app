e31264da33868c6f024cd319625a391675d71d26
tcadamson <taylor@catcat.dev>
2023-01-24 05:44:24 -0700

    style(schema): remove redundant comments

--- recap.sql
+++ recap.sql
@@ -1,6 +1,5 @@
 -- Schema for recap database
 
--- Games data; information about each project
 create table games (
     id integer primary key autoincrement,
     title text collate NOCASE not null unique,
@@ -9,7 +8,6 @@ create table games (
     web text not null
 );
 
--- Posts data; information about each progress post
 -- If no media is attached, the unix timestamp will be lower resolution (no image upload microtime), and the extension will be empty
 create table posts (
     id integer primary key autoincrement,
