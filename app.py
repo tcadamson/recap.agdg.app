6cadf519b86c048a7245c79215a5331e4383648b
tcadamson <taylor@catcat.dev>
2023-05-15 18:42:05 -0600

    refactor(app): replace custom URL converters with flask abort calls

--- app.py
+++ app.py
@@ -4,35 +4,13 @@ import re
 
 from flask import Flask, render_template, request, abort
 from werkzeug.exceptions import HTTPException
-from werkzeug.routing import BaseConverter, ValidationError
 
 import database
 import scraper
 
 STATIC_PATH = "static"
 
-class DatestampConverter(BaseConverter):
-    regex = r"\d{5}"
-
-    def to_python(self, value):
-        # Overriding to_python isn't actually necessary (we don't transform the datestamp into a different object), however we need
-        # it to perform additional validation not possible through regex
-        if glob.glob(f"{STATIC_PATH}/{value}"):
-            return value
-        raise ValidationError
-
-class TitleConverter(BaseConverter):
-    def to_python(self, value):
-        connection = database.Connection(memory = True)
-        if connection.get_row("games", title = value):
-            connection.close()
-            return value
-        raise ValidationError
-
 app = Flask(__name__)
-# https://werkzeug.palletsprojects.com/en/2.2.x/routing/#custom-converters
-app.url_map.converters["datestamp"] = DatestampConverter
-app.url_map.converters["title"] = TitleConverter
 # https://jinja.palletsprojects.com/en/3.0.x/templates/#whitespace-control
 app.jinja_options["trim_blocks"] = True
 app.jinja_options["lstrip_blocks"] = True
@@ -50,10 +28,12 @@ def index():
 def archive():
     return render_template("archive.html.jinja", datestamps = [split_datestamp(x) for x in glob.glob(f"{STATIC_PATH}/*/") if x])
 
-@app.route("/view/<datestamp:datestamp>")
+@app.route("/view/<int:datestamp>")
 def view(datestamp):
     rows = []
     filenames = [re.sub(r".+/(\d+).+", r"\1", x) for x in glob.glob(f"{STATIC_PATH}/{datestamp}/*")]
+    if not filenames:
+        abort(404)
     connection = database.Connection(memory = True)
     cursor = connection.execute(f"""
         select *
@@ -93,11 +73,13 @@ def games():
     connection.close()
     return render_template("games.html.jinja", rows = rows, page = int(request.args.get("page", default = 1)))
 
-@app.route("/games/<title:title>")
+@app.route("/games/<title>")
 def game(title):
     rows = []
     connection = database.Connection(memory = True)
     game_data = connection.get_row("games", title = title)
+    if not game_data:
+        abort(404)
     cursor = connection.execute(f"select * from posts where game_id = ? order by unix desc", (game_data["id"],))
     if cursor:
         rows = cursor.fetchall()
