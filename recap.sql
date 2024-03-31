066717bd5393ba35c359231db7edbe11571ce40f
Taylor Adamson <taylor@catcat.dev>
2024-03-31 17:53:10 -0600

    Rename ID columns to reduce ambiguity and python naming conflicts

--- recap.sql
+++ recap.sql
@@ -1,5 +1,5 @@
 create table game (
-    id integer primary key autoincrement,
+    game_id integer primary key autoincrement,
     title text not null unique collate nocase,
     dev text,
     tools text,
@@ -7,8 +7,8 @@ create table game (
 );
 
 create table post (
-    id integer primary key autoincrement,
-    game_id integer not null references game (id)
+    post_id integer primary key autoincrement,
+    game_id integer not null references game (game_id)
         on update cascade
         on delete cascade,
     unix integer not null,
