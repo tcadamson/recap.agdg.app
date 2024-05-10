bed98426ef51433d2d8224e4109ee9b0db5f1e69
Taylor Adamson <taylor@catcat.dev>
2024-05-09 22:40:46 -0600

    Add missing column defaults and dataclass arguments
    Needed for the generated __init__ method to work as expected.

--- src/weeklyrecap/models.py
+++ src/weeklyrecap/models.py
@@ -11,28 +11,34 @@ _PK = typing.Annotated[int, sql_orm.mapped_column(primary_key=True, autoincremen
 class Game(database.Base):
     """Represents a game in the database."""
 
-    game_id: sql_orm.Mapped[_PK]
+    # Columns
+    game_id: sql_orm.Mapped[_PK] = sql_orm.mapped_column(init=False)
     title: sql_orm.Mapped[str] = sql_orm.mapped_column(
         sql.Text(collation="nocase"), unique=True
     )
-    dev: sql_orm.Mapped[str | None]
-    tools: sql_orm.Mapped[str | None]
-    web: sql_orm.Mapped[str | None]
+    dev: sql_orm.Mapped[str] = sql_orm.mapped_column(default=None)
+    tools: sql_orm.Mapped[str] = sql_orm.mapped_column(default=None)
+    web: sql_orm.Mapped[str] = sql_orm.mapped_column(default=None)
 
+    # Relationships
     posts: sql_orm.Mapped[list["Post"]] = sql_orm.relationship(
-        back_populates="game", cascade="all, delete-orphan"
+        back_populates="game", cascade="all, delete-orphan", default_factory=list
     )
 
 
-class Post(database.Base):
+class Post(database.Base, kw_only=True):
     """Represents a post associated with a game in the database."""
 
-    post_id: sql_orm.Mapped[_PK]
+    # Columns
+    post_id: sql_orm.Mapped[_PK] = sql_orm.mapped_column(init=False)
     game_id: sql_orm.Mapped[int] = sql_orm.mapped_column(
         sql.ForeignKey("game.game_id", onupdate="cascade", ondelete="cascade")
     )
     unix: sql_orm.Mapped[int]
-    filename: sql_orm.Mapped[str | None]
+    filename: sql_orm.Mapped[str] = sql_orm.mapped_column(default=None)
     progress: sql_orm.Mapped[str]
 
-    game: sql_orm.Mapped["Game"] = sql_orm.relationship(back_populates="posts")
+    # Relationships
+    game: sql_orm.Mapped["Game"] = sql_orm.relationship(
+        back_populates="posts", default=None
+    )
