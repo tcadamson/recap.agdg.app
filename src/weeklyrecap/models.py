5ff2c3d0f462a6b63fdeaa27ed61345377171d59
Taylor Adamson <taylor@catcat.dev>
2024-05-09 22:20:41 -0600

    Add Annotated type for primary key columns

--- src/weeklyrecap/models.py
+++ src/weeklyrecap/models.py
@@ -1,15 +1,17 @@
+import typing
+
 import sqlalchemy as sql
 import sqlalchemy.orm as sql_orm
 
 from . import database
 
+_PK = typing.Annotated[int, sql_orm.mapped_column(primary_key=True, autoincrement=True)]
+
 
 class Game(database.Base):
     """Represents a game in the database."""
 
-    game_id: sql_orm.Mapped[int] = sql_orm.mapped_column(
-        primary_key=True, autoincrement=True
-    )
+    game_id: sql_orm.Mapped[_PK]
     title: sql_orm.Mapped[str] = sql_orm.mapped_column(
         sql.Text(collation="nocase"), unique=True
     )
@@ -25,9 +27,7 @@ class Game(database.Base):
 class Post(database.Base):
     """Represents a post associated with a game in the database."""
 
-    post_id: sql_orm.Mapped[int] = sql_orm.mapped_column(
-        primary_key=True, autoincrement=True
-    )
+    post_id: sql_orm.Mapped[_PK]
     game_id: sql_orm.Mapped[int] = sql_orm.mapped_column(
         sql.ForeignKey("game.game_id", onupdate="cascade", ondelete="cascade")
     )
